e06eaa20ef541f9bf9609e6f4ee7fdfab45eb718
mirangee <pmrang@live.co.kr>
2024-05-01 16:12:55 +0900

    리액트 초기 설정 완료 및 Component 개념 확인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import NoName from './NoName';
 
 function App() {
+
+  const $h2 = <h2>반가워요~</h2>; //렌더링 과정에서 HTML로 변환되는 것이지 지금은 HTML이 아닌 JS 문법이다.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <NoName />
+      <div className='App'>
+        <h1>메롱메롱~</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 5월 1일 수요일입니다. <br></br>
+          오후 3시 30분입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 

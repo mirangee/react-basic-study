53b0566528e873b6922a28ec24a26d59d72195af
mirangee <pmrang@live.co.kr>
2024-05-03 16:03:27 +0900

    users 앱 기본 템플릿 작성, module.css 적용

--- src/App.js
+++ src/App.js
@@ -1,76 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
-
-const DUMMY_DATA = [
-  {
-    id: 'g1',
-    text: '리액트 컴포넌트 스타일 마스터하기',
-  },
-  {
-    id: 'g2',
-    text: 'UI 프로그래밍 고수되기',
-  },
-];
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
 
 const App = () => {
-  const [goals, setGoals] = useState(DUMMY_DATA);
-
-  // input에게 전달할 함수
-  const addGoalHandler = (text) => {
-    // console.log('전달받은 텍스트: ', text);
-    const newGoal = {
-      id: Math.random().toString(),
-      text,
-    };
-
-    // 상태변수(배열)를 수정
-    // setGoals([...goals, newGoal]);
-    setGoals((prevGoals) => [...goals, newGoal]);
-  };
-
-  // 삭제 이벤트 핸들러를 CourseItem에게 전달해야 함 (클릭하면 삭제하도록)
-  const deleteGoalHandler = (id) => {
-    // console.log('전달 받은 id', id);
-    // 1. splice로 해당 id 항목 삭제하기
-    // const updateGoals = [...goals]; //상태 배열 그대로 복사해서 가져옴
-    // const index = updateGoals.findIndex((goal) => goal.id === id);
-    // updateGoals.splice(index, 1);
-    // setGoals(updateGoals);
-
-    // 2. filter 고차 함수로 id 항목 뺀 배열만 취하기
-    setGoals(goals.filter((goal) => goal.id !== id));
-
-    // 내가 작성한 콜백함수 스타일
-    // setGoals((prevGoals) => {
-    //   return prevGoals.filter((item) => {
-    //     if (item.id !== id) {
-    //       return item;
-    //     } else {
-    //       return null;
-    //     }
-    //   });
-    // });
-  };
-
-  // CourseList에 조건부 렌더링
-  let listContent = (
-    <p style={{ color: 'red', fontSize: '2em', textAlign: 'center' }}>
-      목표를 등록해 주세요
-    </p>
-  );
-
-  if (goals.length > 0) {
-    listContent = <CourseList items={goals} onDelete={deleteGoalHandler} />;
-  }
-
   return (
     <div>
-      <section id="goal-form">
-        <CourseInput onAdd={addGoalHandler} />
-      </section>
-      <section id="goals">{listContent}</section>
+      <AddUsers />
+      <UserList />
     </div>
   );
 };
---
+++ src/components/Users/UserList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './UserList.module.css';
+import Card from '../UI/Card';
+
+const UserList = () => {
+  return <Card className={styles.users}></Card>;
+};
+export default UserList;

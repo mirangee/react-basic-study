741671ff65343f3c8f4893b679033d6411a8739d
mirangee <pmrang@live.co.kr>
2024-05-07 16:40:26 +0900

    useContext 사용2

--- src/components/SideEffect/MainHeader/MainHeader.js
+++ src/components/SideEffect/MainHeader/MainHeader.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './MainHeader.module.css';
 import Navigation from '../Navigation/Navigation';
 
-const MainHeader = ({ onLogout }) => {
+const MainHeader = () => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={onLogout} />
+      <Navigation />
     </header>
   );
 };

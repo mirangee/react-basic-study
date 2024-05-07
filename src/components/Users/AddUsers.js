e0b77e49df5235303bc01cfd327806e213a99097
mirangee <pmrang@live.co.kr>
2024-05-07 10:22:19 +0900

    Portal 컴포넌트 분할, useRef로 DOM 조작하기

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,40 +1,26 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from '../UI/Button/Button';
 import styles from './AddUsers.module.css';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/Modal/ErrorModal';
 
 const AddUsers = () => {
-  const [userValue, setUserValue] = useState({
-    userName: '',
-    age: '',
-  });
-
   //에러 상태 관리
   const [error, setError] = useState(null);
 
-  const userNameChangeHandler = (e) => {
-    setUserValue((prevUserValue) => {
-      return {
-        ...prevUserValue,
-        userName: e.target.value,
-      };
-    });
-  };
-
-  const ageChangeHandler = (e) => {
-    setUserValue((prevUserValue) => {
-      return {
-        ...prevUserValue,
-        age: e.target.value,
-      };
-    });
-  };
+  // useRef로 기억된 input 요소 가져오기
+  const nameInput = useRef();
+  const ageInput = useRef();
 
   const userSubmitHandler = (e) => {
     e.preventDefault();
+    console.log(nameInput.current);
 
-    if (userValue.userName.trim() === '' || userValue.age.trim() === '') {
+    // 해당 요소의 값 가져오기
+    const username = nameInput.current.value;
+    const age = ageInput.current.value;
+
+    if (username.trim() === '' || age.trim() === '') {
       setError({
         title: '유효하지 않은 입력값',
         message: '입력값이 공백입니다. 내용을 적어주세요',
@@ -42,7 +28,7 @@ const AddUsers = () => {
       return;
     }
 
-    if (+userValue.age < 1) {
+    if (+age < 1) {
       setError({
         title: '유효하지 않은 나이의 범위',
         message: '나이의 입력값은 1 이상이어야 합니다',
@@ -50,12 +36,8 @@ const AddUsers = () => {
       return;
     }
 
-    console.log(userValue);
-
-    setUserValue({
-      userName: '',
-      age: '',
-    });
+    nameInput.current.value = '';
+    ageInput.current.value = '';
   };
 
   return (
@@ -70,19 +52,9 @@ const AddUsers = () => {
       <Card className={styles.input}>
         <form className={styles.input} onSubmit={userSubmitHandler}>
           <label htmlFor="username">이름</label>
-          <input
-            id="username"
-            type="text"
-            onChange={userNameChangeHandler}
-            value={userValue.userName}
-          />
+          <input id="username" type="text" ref={nameInput} />
           <label htmlFor="age">나이</label>
-          <input
-            id="age"
-            type="number"
-            onChange={ageChangeHandler}
-            value={userValue.age}
-          />
+          <input id="age" type="number" ref={ageInput} />
           <Button type="submit">가입하기</Button>
         </form>
       </Card>
